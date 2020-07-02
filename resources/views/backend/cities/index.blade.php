47837b06e334d85c5c7aab28bf77ed8ff60e0f25
Nay Paing Soe <naypaingsoe042@gmail.com>
2020-07-02 14:21:47 -0400

    city seeder

---
+++ resources/views/backend/cities/index.blade.php
@@ -0,0 +1,72 @@
+@extends('backend.master')
+
+@section('style')
+<link href="{{asset('backendtemplate/vendor/datatables/dataTables.bootstrap4.min.css')}}" rel="stylesheet">
+@endsection
+
+@section('content')
+
+<div class="mb-3">
+	<span class="h3 text-gray-800">Subcategory</span><a href="{{ route('subcategories.create') }}"class="btn btn-primary float-right">Add New</a>
+</div>
+<div class="card shadow mb-4">
+	<div class="card-header py-3">
+		<h6 class="m-0 font-weight-bold text-primary"> Subcategory lists</h6>
+	</div>
+ <div class="card-body">
+  <div class="table-responsive">
+    <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+      <thead>
+        <tr>
+         <th>No.</th>
+         <th>Name</th>
+         <th>Category</th>
+         <th>Action</th>
+
+       </tr>
+     </thead>
+     <tfoot>
+      <tr>
+        <th>No.</th>
+        <th>Name</th>
+        <th>Category</th>
+        <th>Action</th>
+
+      </tr>
+    </tfoot>
+    <tbody>
+
+     @php $i=1;@endphp
+     @foreach($subcategories as $row)
+     <tr>
+       <th>{{$i++}}</th>
+       <th>
+        {{$row->name}}
+      </th>
+      <th>
+        
+       {{$row->category_id}}</th>
+       <th>
+        <a href="{{route('subcategories.edit',$row->id)}}" class="btn btn-warning" >Edit</a>
+        <form style="display: inline;" method="post" action="{{route('subcategories.destroy',$row->id)}}"  onsubmit="return comfirm('Are you comfirm')">
+           @csrf
+           @method('DELETE')
+           <input type="submit" name="btnsubmit" value="Delete" class="btn btn-danger">
+         </form>
+       </th>
+
+     </tr>
+     @endforeach
+   </tbody>
+ </table> 
+</div>
+</div>
+</div>
+
+
+@endsection
+@section('script')
+<script src="{{asset('backendtemplate/vendor/datatables/jquery.dataTables.min.js')}}"></script>
+<script src="{{asset('backendtemplate/vendor/datatables/dataTables.bootstrap4.min.js')}}"></script>
+<script src="{{asset('backendtemplate/js/demo/datatables-demo.js')}}"></script>
+@endsection
